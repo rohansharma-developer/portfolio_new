cd893765326a7d0f1b00af463b30cda7df89434e
Rohan Sharma <107614947+rohansharma-developer@users.noreply.github.com>
2024-02-29 12:39:04 +0000

    added fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,111 @@
-let animationContainer = document.querySelector('.animated-title');
-let textData = animationContainer.getAttribute('aria-label');
-
-function splitWords() {
-  let splitedText = textData.split(' ');
-  
-  splitedText.join('& &').split('&').forEach(function(e){
-    let span = document.createElement('span');
-    span.classList.add('animated-word');
-    span.setAttribute('data-text', e); 
-    animationContainer.appendChild(span);
-  });
-  splitLetters();
+function r(from, to) {
+  return ~~(Math.random() * (to - from + 1) + from);
 }
-splitWords()
-
-
-function splitLetters() {
-  let animatedWords = document.querySelectorAll('.animated-word');
-  animatedWords.forEach(function(e, i){
-    e.getAttribute('data-text').split('').forEach(function(f, j) {
-      f = f == ' ' ? '&#32;' : f;
-      let span = document.createElement('span');
-      span.classList.add('animated-element');
-      span.setAttribute('aria-hidden', 'true'); 
-      span.innerHTML = f;
-      e.appendChild(span);
+function pick() {
+  return arguments[r(0, arguments.length - 1)];
+}
+function getChar() {
+  return String.fromCharCode(pick(
+    r(0x3041, 0x30ff),
+    r(0x2000, 0x206f),
+    r(0x0020, 0x003f)
+  ));
+}
+function loop(fn, delay) {
+  let stamp = Date.now();
+  function _loop() {
+    if (Date.now() - stamp >= delay) {
+      fn(); stamp = Date.now();
+    }
+    requestAnimationFrame(_loop);
+  }
+  requestAnimationFrame(_loop);
+}
+class Char {
+  constructor() {
+    this.element = document.createElement('span');
+    this.mutate();
+  }
+  mutate() {
+    this.element.textContent = getChar();
+  }
+}
+class Trail {
+  constructor(list = [], options) {
+    this.list = list;
+    this.options = Object.assign(
+      { size: 20, offset: 0 }, options
+    );
+    this.body = [];
+    this.move();
+  }
+  traverse(fn) {
+    this.body.forEach((n, i) => {
+      let last = (i == this.body.length - 1);
+      if (n) fn(n, i, last);
     });
-    animate(e, i);
-  })
+  }
+  move() {
+    this.body = [];
+    let { offset, size } = this.options;
+    for (let i = 0; i < size; ++i) {
+      let item = this.list[offset + i - size + 1];
+      this.body.push(item);
+    }
+    this.options.offset = 
+      (offset + 1) % (this.list.length + size - 1);
+  }
+}
+class Rain {
+  constructor({ target, row }) {
+    this.element = document.createElement('p');
+    this.build(row);
+    if (target) {
+      target.appendChild(this.element);
+    }
+    this.drop();
+  }
+  build(row = 20) {
+    let root = document.createDocumentFragment();
+    let chars = [];
+    for (let i = 0; i < row; ++i) {
+      let c = new Char();
+      root.appendChild(c.element);
+      chars.push(c);
+      if (Math.random() < .5) {
+        loop(() => c.mutate(), r(1e3, 5e3));
+      }
+    }
+    this.trail = new Trail(chars, { 
+      size: r(10, 30), offset: r(0, 100) 
+    });
+    this.element.appendChild(root); 
+  }
+  drop() {
+    let trail = this.trail;
+    let len = trail.body.length;
+    let delay = r(10, 100);
+    loop(() => {
+      trail.move();
+      trail.traverse((c, i, last) => {
+        c.element.style = `
+          color: hsl(136, 100%, ${85 / len * (i + 1)}%)
+        `;
+        if (last) {
+          c.mutate();
+          c.element.style = `
+            color: hsl(136, 100%, 85%);
+            text-shadow:
+              0 0 .5em #fff,
+              0 0 .5em currentColor;
+          `;
+        }
+      });
+    }, delay);
+  }
 }
 
-function animate(e, i) {
-  let wordCount = e.getAttribute('data-text').length;
-  e.style.opacity = 1;
-  e.classList.add('animate');
-}
-
-// Replay Button - For Demo purpose only
-function replay() {
-  let animatedWords = document.querySelectorAll('.animated-word');
-  animatedWords.forEach(function(e, i){
-    e.classList.remove('animate');
-    e.style.opacity = 0;
-    setTimeout(() => {
-      animate(e, i);
-    }, 500);
-  })
+const main = document.querySelector('main');
+for (let i = 0; i < 100; ++i) {
+  new Rain({ target: main, row: 80 });
 }
