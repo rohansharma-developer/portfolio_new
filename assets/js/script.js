4a5e3ef9332b38113cf02c6fad7de05201e50b12
Rohan Sharma <107614947+rohansharma-developer@users.noreply.github.com>
2024-03-01 19:21:22 +0000

    added projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 
+$(document).on("click", ".naccs .nav-menu div", function() {
+	var numberIndex = $(this).index();
+
+	if (!$(this).is("active")) {
+		$(".naccs .nav-menu div").removeClass("active");
+		$(".naccs ul li").removeClass("active");
+
+		$(this).addClass("active");
+		$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+		var listItemHeight = $(".naccs ul")
+			.find("li:eq(" + numberIndex + ")")
+			.innerHeight();
+		$(".naccs ul").height(listItemHeight + "px");
+	}
+});
 
 const m = document.querySelector("#m");
 const k = document.querySelector("#k");
