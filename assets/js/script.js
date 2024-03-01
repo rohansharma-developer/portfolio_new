9eb47525913ded2572a9d9c200deef648d3f62d8
Rohan Sharma <107614947+rohansharma-developer@users.noreply.github.com>
2024-03-01 16:27:12 +0000

    some changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,106 @@
-function r(from, to) {
-  return ~~(Math.random() * (to - from + 1) + from);
-}
-function pick() {
-  return arguments[r(0, arguments.length - 1)];
-}
-function getChar() {
-  return String.fromCharCode(pick(
-    r(0x3041, 0x30ff),
-    r(0x2000, 0x206f),
-    r(0x0020, 0x003f)
-  ));
-}
-function loop(fn, delay) {
-  let stamp = Date.now();
-  function _loop() {
-    if (Date.now() - stamp >= delay) {
-      fn(); stamp = Date.now();
-    }
-    requestAnimationFrame(_loop);
-  }
-  requestAnimationFrame(_loop);
-}
-class Char {
-  constructor() {
-    this.element = document.createElement('span');
-    this.mutate();
-  }
-  mutate() {
-    this.element.textContent = getChar();
-  }
-}
-class Trail {
-  constructor(list = [], options) {
-    this.list = list;
-    this.options = Object.assign(
-      { size: 20, offset: 0 }, options
-    );
-    this.body = [];
-    this.move();
-  }
-  traverse(fn) {
-    this.body.forEach((n, i) => {
-      let last = (i == this.body.length - 1);
-      if (n) fn(n, i, last);
-    });
-  }
-  move() {
-    this.body = [];
-    let { offset, size } = this.options;
-    for (let i = 0; i < size; ++i) {
-      let item = this.list[offset + i - size + 1];
-      this.body.push(item);
-    }
-    this.options.offset = 
-      (offset + 1) % (this.list.length + size - 1);
-  }
-}
-class Rain {
-  constructor({ target, row }) {
-    this.element = document.createElement('p');
-    this.build(row);
-    if (target) {
-      target.appendChild(this.element);
-    }
-    this.drop();
-  }
-  build(row = 20) {
-    let root = document.createDocumentFragment();
-    let chars = [];
-    for (let i = 0; i < row; ++i) {
-      let c = new Char();
-      root.appendChild(c.element);
-      chars.push(c);
-      if (Math.random() < .5) {
-        loop(() => c.mutate(), r(1e3, 5e3));
-      }
-    }
-    this.trail = new Trail(chars, { 
-      size: r(10, 30), offset: r(0, 100) 
-    });
-    this.element.appendChild(root); 
-  }
-  drop() {
-    let trail = this.trail;
-    let len = trail.body.length;
-    let delay = r(10, 100);
-    loop(() => {
-      trail.move();
-      trail.traverse((c, i, last) => {
-        c.element.style = `
-          color: hsl(136, 100%, ${85 / len * (i + 1)}%)
-        `;
-        if (last) {
-          c.mutate();
-          c.element.style = `
-            color: hsl(136, 100%, 85%);
-            text-shadow:
-              0 0 .5em #fff,
-              0 0 .5em currentColor;
-          `;
-        }
-      });
-    }, delay);
-  }
+/*
+Designed by: Miguel E.
+Original image: https://dribbble.com/shots/6276517-Hello
+*/
+
+const m = document.querySelector("#m");
+const k = document.querySelector("#k");
+const s = document.querySelector("#s");
+
+const kd = document.querySelectorAll(".key")
+let con = 0;
+
+let base = (e) => {
+    let  x = e.pageX / window.innerWidth - 0.5;
+    let  y = e.pageY / window.innerHeight - 0.5;
+    k.style.transform = `
+        perspective(10000px)
+        rotateX(${ y * 10  + 60}deg)
+        rotateZ(-${ x * 40  + 35}deg)
+    `;
 }
 
-const main = document.querySelector('main');
-for (let i = 0; i < 100; ++i) {
-  new Rain({ target: main, row: 80 });
+let addKey = (e) => {
+    let kc = event.keyCode;
+
+    if ( (kc >= 65 && kc <= 90) || kc == 32) {
+        if (kc == 81) { kd[15].classList.add("key--down"); }
+        else if (kc == 87) { kd[16].classList.add("key--down"); }
+        else if (kc == 69) { kd[17].classList.add("key--down"); }
+        else if (kc == 82) { kd[18].classList.add("key--down"); }
+        else if (kc == 84) { kd[19].classList.add("key--down"); }
+        else if (kc == 89) { kd[20].classList.add("key--down"); }
+        else if (kc == 85) { kd[21].classList.add("key--down"); }
+        else if (kc == 73) { kd[22].classList.add("key--down"); }
+        else if (kc == 79) { kd[23].classList.add("key--down"); }
+        else if (kc == 80) { kd[24].classList.add("key--down"); }
+        else if (kc == 65) { kd[29].classList.add("key--down"); }
+        else if (kc == 83) { kd[30].classList.add("key--down"); }
+        else if (kc == 68) { kd[31].classList.add("key--down"); }
+        else if (kc == 70) { kd[32].classList.add("key--down"); }
+        else if (kc == 71) { kd[33].classList.add("key--down"); }
+        else if (kc == 72) { kd[34].classList.add("key--down"); }
+        else if (kc == 74) { kd[35].classList.add("key--down"); }
+        else if (kc == 75) { kd[36].classList.add("key--down"); }
+        else if (kc == 76) { kd[37].classList.add("key--down"); }
+        else if (kc == 192) { kd[38].classList.add("key--down"); }
+        else if (kc == 90) { kd[41].classList.add("key--down"); }
+        else if (kc == 88) { kd[42].classList.add("key--down"); }
+        else if (kc == 67) { kd[43].classList.add("key--down"); }
+        else if (kc == 86) { kd[44].classList.add("key--down"); }
+        else if (kc == 66) { kd[45].classList.add("key--down"); }
+        else if (kc == 78) { kd[46].classList.add("key--down"); }
+        else if (kc == 77) { kd[47].classList.add("key--down"); }
+        else if (kc == 13) { kd[39].classList.add("key--down"); }
+        else if (kc == 32) {
+            kd[56].classList.add("key--down");
+            s.innerHTML += "&nbsp;";
+        }
+        s.innerHTML += String.fromCharCode(kc);
+        con++;
+        if (con > 10) { s.innerHTML = ""; con = 0; }
+    }
+    if (kc == 8) {
+        s.innerHTML = "";
+        kd[27].classList.add("key--down");
+			  con = 0;
+    }
+
 }
+let removeKey = (e) => {
+    let kc = event.keyCode;
+    if (kc == 81) { kd[15].classList.remove("key--down"); }
+    else if (kc == 87) { kd[16].classList.remove("key--down"); }
+    else if (kc == 69) { kd[17].classList.remove("key--down"); }
+    else if (kc == 82) { kd[18].classList.remove("key--down"); }
+    else if (kc == 84) { kd[19].classList.remove("key--down"); }
+    else if (kc == 89) { kd[20].classList.remove("key--down"); }
+    else if (kc == 85) { kd[21].classList.remove("key--down"); }
+    else if (kc == 73) { kd[22].classList.remove("key--down"); }
+    else if (kc == 79) { kd[23].classList.remove("key--down"); }
+    else if (kc == 80) { kd[24].classList.remove("key--down"); }
+    else if (kc == 65) { kd[29].classList.remove("key--down"); }
+    else if (kc == 83) { kd[30].classList.remove("key--down"); }
+    else if (kc == 68) { kd[31].classList.remove("key--down"); }
+    else if (kc == 70) { kd[32].classList.remove("key--down"); }
+    else if (kc == 71) { kd[33].classList.remove("key--down"); }
+    else if (kc == 72) { kd[34].classList.remove("key--down"); }
+    else if (kc == 74) { kd[35].classList.remove("key--down"); }
+    else if (kc == 75) { kd[36].classList.remove("key--down"); }
+    else if (kc == 76) { kd[37].classList.remove("key--down"); }
+    else if (kc == 192) { kd[38].classList.remove("key--down"); }
+    else if (kc == 90) { kd[41].classList.remove("key--down"); }
+    else if (kc == 88) { kd[42].classList.remove("key--down"); }
+    else if (kc == 67) { kd[43].classList.remove("key--down"); }
+    else if (kc == 86) { kd[44].classList.remove("key--down"); }
+    else if (kc == 66) { kd[45].classList.remove("key--down"); }
+    else if (kc == 78) { kd[46].classList.remove("key--down"); }
+    else if (kc == 77) { kd[47].classList.remove("key--down"); }
+    else if (kc == 32) { kd[56].classList.remove("key--down"); }
+    else if (kc == 13) { kd[39].classList.remove("key--down"); }
+    else if (kc == 8) { kd[27].classList.remove("key--down"); }
+}
+
+m.addEventListener("mousemove", base);
+window.addEventListener("keydown", addKey);
+window.addEventListener("keyup", removeKey);
